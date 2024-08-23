e19fc1f69cfcafac0bb62f0da323c76e0da554b6
peterFlores <2010330pedroflores@gmail.com>
2024-08-23 00:27:37 -0600

    Update MarketsController.java

--- src/main/java/com/pflores/msmarkets/controller/MarketsController.java
+++ src/main/java/com/pflores/msmarkets/controller/MarketsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@RequestMapping("/v1")
+@RequestMapping("/ms-markets")
 @RestController
 public class MarketsController {
 
